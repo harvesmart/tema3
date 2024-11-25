ff39951aca3de6c169626c8e2f64394da9c0c0cf
harvesmart <harvesmart@gmail.com>
2024-11-26 05:50:07 +0900

    tema3

---
+++ MyApp/MyApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using MyLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMethods methods = new MyMethods();
+
+            methods.PrintMessage();
+            methods.DisplayDate();
+
+            int sum = methods.AddNumbers(13, 37);
+            Console.WriteLine($"Сумма чисел: {sum}");
+
+            string greeting = methods.GetGreeting("Андрей");
+            Console.WriteLine(greeting);
+        }
+    }
+}
---
+++ MyLibrary/MyLibrary/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class MyMethods
+    {
+        public void PrintMessage()
+        {
+            Console.WriteLine("Акууууна матата");
+        }
+
+        public void DisplayDate()
+        {
+            Console.WriteLine($"Сегодняшняя дата: {DateTime.Now.ToShortDateString()}");
+        }
+
+        public int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+
+        public string GetGreeting(string name)
+        {
+            return $"Привет, {name}";
+        }
+    }
+}
